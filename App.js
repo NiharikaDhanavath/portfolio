1c42cb4cc22e244809240f8531cec7f39517d264
Niharika Dhanavath <144577406+NiharikaDhanavath@users.noreply.github.com>
2024-11-14 11:44:40 -0500

    App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Works from './Works'; // Make sure the path is correct based on your folder structure
+
+function App() {
+  return (
+    <div className="App">
+      {/* Other components like Navbar, Hero, About, etc. */}
+      <Works />
+      {/* Other components like Footer, etc. */}
+    </div>
+  );
+}
+
+export default App;
